567483f45993c909a23633e3c6a3bbafdd9f9dbc
Julien Schueller <schueller@phimeca.com>
2017-12-22 17:13:01 +0100

    Update to 1.66

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -25,7 +25,7 @@ if errorlevel 1 exit 1
 if errorlevel 1 exit 1
 
 :: Get the major minor version info (e.g. `1_61`)
-python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:3]))" > temp.txt
+python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
 set /p MAJ_MIN_VER=<temp.txt
 
 :: Install fix-up for a non version-specific boost include
@@ -33,6 +33,6 @@ xcopy /E/Y %LIBRARY_INC%\boost-%MAJ_MIN_VER%\boost\* %LIBRARY_INC%\boost\
 rmdir /s /q %LIBRARY_INC%\boost-%MAJ_MIN_VER%\boost
 
 :: Move dll's to LIBRARY_BIN
-move %LIBRARY_LIB%\*vc%VS_MAJOR%0-mt-%MAJ_MIN_VER%.dll "%LIBRARY_BIN%"
+move %LIBRARY_LIB%\*vc%VS_MAJOR%0-mt-x%ARCH%-%MAJ_MIN_VER%.dll "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
 
