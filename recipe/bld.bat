121da3296195ee88d309af15b3a175803eec0792
Isuru Fernando <isuruf@gmail.com>
2017-12-25 14:49:50 +0530

    boost include dirs are now non-version specific

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -16,14 +16,6 @@ if errorlevel 1 exit 1
     --without-python
 if errorlevel 1 exit 1
 
-:: Get the major minor version info (e.g. `1_61`)
-python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
-set /p MAJ_MIN_VER=<temp.txt
-
-:: Install fix-up for a non version-specific boost include
-move %LIBRARY_INC%\boost-%MAJ_MIN_VER%\boost %LIBRARY_INC%
-if errorlevel 1 exit 1
-
 :: Remove Python headers as we don't build Boost.Python.
 del %LIBRARY_INC%\boost\python.hpp
 rmdir /s /q %LIBRARY_INC%\boost\python
@@ -32,6 +24,7 @@ rmdir /s /q %LIBRARY_INC%\boost\python
 move %LIBRARY_LIB%\boost*.dll "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
 
+:: Set BOOST_AUTO_LINK_NOMANGLE so that auto-linking uses system layout
 echo &echo.                           >> %LIBRARY_INC%\boost\config\user.hpp
 echo #define BOOST_AUTO_LINK_NOMANGLE >> %LIBRARY_INC%\boost\config\user.hpp
 
