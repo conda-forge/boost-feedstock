5d9a33b7c6fe383fa62994839e9f4ef4297a0428
Julien Schueller <schueller@phimeca.com>
2019-04-30 16:37:39 +0200

    Omit toolset version string

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,7 +25,7 @@ fi
 
 # http://www.boost.org/build/doc/html/bbv2/tasks/crosscompile.html
 cat <<EOF > ${SRC_DIR}/tools/build/src/site-config.jam
-using ${TOOLSET} : custom : ${CXX} ;
+using ${TOOLSET} : : ${CXX} ;
 EOF
 
 LINKFLAGS="${LINKFLAGS} -L${LIBRARY_PATH}"
@@ -50,7 +50,7 @@ sed -i.bak "s,cc,${TOOLSET},g" ${SRC_DIR}/project-config.jam
     threading=multi \
     runtime-link=shared \
     link=static,shared \
-    toolset=${TOOLSET}-custom \
+    toolset=${TOOLSET} \
     python="${PY_VER}" \
     include="${INCLUDE_PATH}" \
     cxxflags="${CXXFLAGS}" \
