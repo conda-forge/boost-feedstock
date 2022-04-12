1dfd012c92f097b2d20c2f890ccc3bd5fe6a5cbb
Ben Webb <ben@salilab.org>
2022-04-12 15:48:10 -0700

    Chain compile+execute
    If the compile fails, it doesn't make sense
    to try to run the non-existent binary; the
    error message will be harder to interpet.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,5 +5,4 @@ fi
 
 # Test boost::iostreams zlib filter support
 cd test
-${CXX} -I$PREFIX/include -L$PREFIX/lib test_iostreams_zlib.cpp -lboost_iostreams
-./a.out
+${CXX} -I$PREFIX/include -L$PREFIX/lib test_iostreams_zlib.cpp -lboost_iostreams && ./a.out
