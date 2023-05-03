dd20cafa94443845838deaf0af94f265626a83c0
H. Vetinari <h.vetinari@gmx.com>
2023-05-04 10:37:41 +1100

    silence robocopy

--- recipe/install-lib.bat
+++ recipe/install-lib.bat
@@ -1,11 +1,11 @@
 @echo on
 
 :: for libboost-headers, only the headers
-robocopy temp_prefix\include %LIBRARY_INC% /E
+robocopy temp_prefix\include %LIBRARY_INC% /E >nul
 
 :: everything else
 if [%PKG_NAME%] == [libboost] (
-    robocopy temp_prefix\lib %LIBRARY_LIB% /E
+    robocopy temp_prefix\lib %LIBRARY_LIB% /E >nul
     REM Move dll's to LIBRARY_BIN
     move %LIBRARY_LIB%\boost*.dll "%LIBRARY_BIN%"
 )
