0923fa930b81d68c6ce7555cc410fdcac056961a
Marcel Bargull <marcel.bargull@udo.edu>
2024-03-25 12:18:14 +0100

    cb-24.3
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -26,9 +26,9 @@ export CONDA_SOLVER="libmamba"
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
 mamba install --update-specs --quiet --yes --channel conda-forge/label/lief_rc --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1" "py-lief>=0.14.1"
+    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.3" "py-lief>=0.14.1"
 mamba update --update-specs --yes --quiet --channel conda-forge/label/lief_rc --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1" "py-lief>=0.14.1"
+    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.3" "py-lief>=0.14.1"
 
 
 
