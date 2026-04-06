c6a0588e0272580bf8294b80d1abc92bb0a397d2
Julianus Pfeuffer <8102638+jpfeuffer@users.noreply.github.com>
2026-04-06 20:21:00 +0200

    Fix error handling in install-lib ??

--- recipe/install-lib.bat
+++ recipe/install-lib.bat
@@ -15,6 +15,6 @@ if [%PKG_NAME%] == [libboost-headers] (
     REM everything else
     xcopy /E /Y temp_prefix\lib %LIBRARY_LIB%
     REM Patch CMake configs so IMPORTED_LOCATION for DLLs points to bin/ not lib/
-    python "%RECIPE_DIR%\fix-cmake-dll-paths.py" %LIBRARY_LIB%\cmake
-    if %ERRORLEVEL% neq 0 exit 1
+    python "%RECIPE_DIR%\fix-cmake-dll-paths.py" "%LIBRARY_LIB%\cmake"
+    if errorlevel 1 exit /b 1
 )
