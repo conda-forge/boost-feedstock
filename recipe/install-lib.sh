035711009489217e58176f3753c2c3052b08c2b6
H. Vetinari <h.vetinari@gmx.com>
2026-04-23 07:49:36 +1100

    remove libboost_regex.a before copying to PREFIX

--- recipe/install-lib.sh
+++ recipe/install-lib.sh
@@ -5,6 +5,8 @@ if [[ "$PKG_NAME" == "libboost-headers" ]]; then
     # for libboost-headers, only the headers;
     cp -R temp_prefix/include/. $PREFIX/include
 elif [[ "$PKG_NAME" == "libboost" ]]; then
+    # remove static variant that exists in addition to shared variant
+    rm -f temp_prefix/lib/libboost_regex.a
     # only the libraries (don't copy CMake metadata)
     cp -R temp_prefix/lib/libboost*${SHLIB_EXT}* $PREFIX/lib
     cp -R temp_prefix/lib/libboost*.a $PREFIX/lib
